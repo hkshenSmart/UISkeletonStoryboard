153096a330fc0469a1e071d1ed01f592b857d778
hkshen <308421189@qq.com>
2017-03-13 16:36:06 +0800

    Initial Commit

---
+++ UISkeletonStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISkeletonStoryboard
+//
+//  Created by kun shen on 2017/3/13.
+//  Copyright © 2017年 kun shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
