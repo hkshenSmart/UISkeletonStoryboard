e9c5bcb1c805823d3b5bebcbbbc7b84e818f907d
hkshen <308421189@qq.com>
2017-03-15 10:53:41 +0800

    Remote notification Extension.

---
+++ NotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by kun shen on 2017/3/15.
+//  Copyright © 2017年 kun shen. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
